687c96185ceca9bcbe9f9148e1234915463a5db5
Takahiro55555 <tkhr.corei7@gmail.com>
2020-04-13 18:52:10 +0900

    Update: 標準入力で入力したパスワードをハッシュ化し、表示できるようになった

--- src/module/password_hash.py
+++ src/module/password_hash.py
@@ -1,5 +1,3 @@
-import sys
-
 import bcrypt
 
 def hash_password(password, rounds=12):
@@ -10,4 +8,6 @@ def check_password(user_password, hashed_password):
     return bcrypt.checkpw(user_password.encode(), hashed_password.encode())
 
 if __name__ == "__main__":
-    pass
+    from getpass import getpass
+    hashed_password = hash_password(getpass())
+    print(hashed_password)
